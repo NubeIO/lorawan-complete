6d1c504c0db30248166414a181f137cb198ea5c3
Shiny380 <dmc@nube-io.com>
2021-06-10 14:14:24 +1000

    group band 0 and 1 channels

--- chirpstack-app-init.py
+++ chirpstack-app-init.py
@@ -15,12 +15,13 @@ else:
     exit(1)
 
 if channel_band == '0':
-    channels = [0, 1, 2, 3, 4, 5, 6, 7, 64]
+#     channels = [0, 1, 2, 3, 4, 5, 6, 7, 64]
 elif channel_band == '1':
-    channels = [8, 9, 10, 11, 12, 13, 14, 15, 65]
+#     channels = [8, 9, 10, 11, 12, 13, 14, 15, 65]
 else:
     print('Channel band not supported')
     exit(1)
+channels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 64, 65]
 
 # login
 resp = requests.post('http://127.0.0.1:8080/api/internal/login',
